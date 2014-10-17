415f58984259e2d76862fb1ff009beba36e7d852
Daniel Dickinson <ddickins@live.unc.edu>
2014-10-17 16:09:55 -0400

    Test database query
    Former-commit-id: 3b5347fba44bdabc1706e0868ce3dff434c12045

---
+++ web-optimizer.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use 5.010;
+use CGI;
+use BerkeleyDB;
+use Math::Random;
+use lib '/libs/Seqscore.pm';
+
+my $sequence_lib = new BerkeleyDB::Btree
+    -Filename => '/libs/sequence_lib_scores.db';
+
+my $q = CGI->new();
+say $q->header(), $q->start_html();
+
+say "<h1>Results</h1>";
+
+print "<p>Processing Input...</p>";
+
+my $inseq = $q->param('sequence');
+my $safeseq = $q->escapeHTML($inseq);
+
+say "<p> Input: $safeseq";
+$sequence_lib -> db_get($safeseq, my $score);
+say "Score: $score </p>";
+
+say $q->end_html();
