cf7cff399210f2583d83f15fda09e21333b86eb1
Daniel Dickinson <ddickins@live.unc.edu>
2014-11-04 15:38:53 -0500

    Try validating the AA sequence

--- web-optimizer.pl
+++ web-optimizer.pl
@@ -5,6 +5,7 @@ use warnings;
 use 5.010;
 use CGI;
 use BerkeleyDB;
+use Bio::Seq;
 use Math::Random;
 use lib '/libs/Seqscore.pm';
 
@@ -17,23 +18,41 @@ my $sequence_lib = new BerkeleyDB::Btree
 my $q = CGI->new();
 say $q->header(), $q->start_html();
 
+# Get input sequence
+my $inseq = $q->param('sequence');
+my $safeseq = $q->escapeHTML($inseq);
+my $seqobj = Bio::Seq->new( -seq => $safeseq );
+my $seqtype = $q->param('seq_type');
+
+# Check sequence; if DNA input, convert to AA
+if ($seqtype eq 'AA') {
+    if ( $seqobj->alphabet ne 'protein' ) {
+        error("You selected \"Amino Acid\", but your input doesn't appear to be an amino acid sequence. Please check the sequence and try again");
+    }
+    if ( ! $seqobj->validate_seq($seqobj->seq()) ) {
+        error("You entered an invalid sequence");
+    }
+}
+
+
 # Process and display sequence name
 my $in_name = $q->param('name');
 my $safename = $q->escapeHTML($in_name);
 if ( $safename ) {
-    say "<h2>Results for sequence $safename</h2>";
+    say "<h2>Results for sequence \"$safename\"</h2>";
 } else {
     say "<h2>Results</h2>";
 }
-
-
-print "<p>Processing Input...</p>";
-
-my $inseq = $q->param('sequence');
-my $safeseq = $q->escapeHTML($inseq);
 
 say "<p> Input: $safeseq";
 $sequence_lib -> db_get($safeseq, my $score);
 say "Score: $score </p>";
 
 say $q->end_html();
+
+sub error(my $errormsg) {
+    say("<h2>Error</h2>");
+    say("<p>$errormsg</p>");
+    say $q->end_html();
+    die;
+}
