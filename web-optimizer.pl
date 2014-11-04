90f269738e55554e74d05e72e6406c59a8a61e6d
Daniel Dickinson <ddickins@live.unc.edu>
2014-11-04 17:33:49 -0500

    Try validating the AA sequence

--- web-optimizer.pl
+++ web-optimizer.pl
@@ -22,8 +22,6 @@ say $q->header(), $q->start_html();
 my $inseq = $q->param('sequence');
 my $safeseq = $q->escapeHTML($inseq);
 $safeseq =~ s/\s+//g; #Remove whitespace
-
-say "<p>$safeseq OK here</p>";
 my $seqtype = $q->param('seq_type');
 
 # Check sequence for invalid characters
@@ -33,14 +31,24 @@ if ( ! $bio_seq->validate_seq($safeseq) ) {
     error("You entered an invalid sequence");
 }
 
+# If sequence is ok, make a Bio::Seq object out of it
 my $seqobj = Bio::Seq->new( -sequence => $safeseq );
 
+# Check sequence for correct type, generate amino acid sequence
+my $AAseq;
 if ($seqtype eq 'AA') {
     if ( $seqobj->alphabet ne 'protein' ) {
-        error("You selected \"Amino Acid\", but your input doesn't appear to be an amino acid sequence. Please check the sequence and try again");
+        error("You selected \"Amino Acid,\" but your input doesn't appear to be an amino acid sequence. Please check the sequence and try again");
     }
+    $AAseq = $seqobj->seq();
+} elsif ($seqtype eq 'DNA') {
+    if ( $seqobj->alphaet ne 'dna') {
+        error("You selected \"DNA,\" but your input doesn't appear to be a nucleotide sequence. Please check the sequence and try again");
+    }
+    $AAseq = $seqobj->translate();
+} else {
+    error("Program error :-\("); #You'd only get this if the HTML form returned the wrong value.
 }
-
 
 # Process and display sequence name
 my $in_name = $q->param('name');
