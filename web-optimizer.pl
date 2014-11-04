b4af4c1922c6811c65587e473e4cd409dcce80dc
Daniel Dickinson <ddickins@live.unc.edu>
2014-11-04 17:37:24 -0500

    Try validating the AA sequence

--- web-optimizer.pl
+++ web-optimizer.pl
@@ -23,6 +23,7 @@ my $inseq = $q->param('sequence');
 my $safeseq = $q->escapeHTML($inseq);
 $safeseq =~ s/\s+//g; #Remove whitespace
 my $seqtype = $q->param('seq_type');
+say "<p>$safeseq</p>";
 
 # Check sequence for invalid characters
 my $bio_seq = Bio::Seq->new();
@@ -41,11 +42,13 @@ if ($seqtype eq 'AA') {
         error("You selected \"Amino Acid,\" but your input doesn't appear to be an amino acid sequence. Please check the sequence and try again");
     }
     $AAseq = $seqobj->seq();
+    say "<p>$AAseq</p>";
 } elsif ($seqtype eq 'DNA') {
     if ( $seqobj->alphaet ne 'dna') {
         error("You selected \"DNA,\" but your input doesn't appear to be a nucleotide sequence. Please check the sequence and try again");
     }
     $AAseq = $seqobj->translate();
+    say "<p>$AAseq</p>";
 } else {
     error("Program error :-\("); #You'd only get this if the HTML form returned the wrong value.
 }
