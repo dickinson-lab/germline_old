74b9cce83f4219e80d3089edfb7ee94aa980ae6d
Daniel Dickinson <ddickins@live.unc.edu>
2014-11-12 11:04:35 -0500

    Playing with watching a long process

--- fork-example.pl
+++ fork-example.pl
@@ -3,6 +3,7 @@ use strict;
 use CGI::Pretty qw(:standard :cgi-lib);
 use CGI::Carp qw(fatalsToBrowser); # Remove for production code
 use CGI::Session;
+use CGI qw(:all delete_all escapeHTML);
 
 $CGI::DISABLE_UPLOADS = 1;         # Disable uploads
 $CGI::POST_MAX        = 10240; # Maximum number of bytes per post
@@ -20,7 +21,7 @@ if (param('Spawn')) {
     #FORK
     defined (my $kid = fork) or die "Cannot fork: $!\n";
     if ($kid) {
-        Delete_all();
+        delete_all();
         param('session', $session);
         print redirect (self_url());
     } else {
