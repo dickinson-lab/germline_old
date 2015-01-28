c169252eb3ad9f428cf58cc1a0443bb1e166efbf
Daniel Dickinson <ddickins@live.unc.edu>
2015-01-28 09:20:48 -0500

    Change OptimizerMain path

--- libs/OptimizerMain.pm
+++ libs/OptimizerMain.pm
@@ -19,7 +19,7 @@ sub start_optimization : StartRunmode {
     
     if (my $pid = fork) {
         # parent does this
-        return $self->redirect("/libs/OptimizerMain.pm/optimizer_status");
+        return $self->redirect("/optimize-start.pl/optimizer_status");
     } elsif (defined $pid) {
         # child does this
         close STDOUT;
