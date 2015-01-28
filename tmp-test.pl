c0035ffcb3ae45a411d545d939a17585470c652b
Daniel Dickinson <ddickins@live.unc.edu>
2015-01-28 15:51:20 -0500

    Test creating and reading a tmp file

---
+++ tmp-test.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use HTML::Template;
+
+my $tmpdir = $ENV{OPENSHIFT_TMP_DIR};
+open OUTPUT, ">", "$tmpdir" . 'results.txt';
+print OUTPUT "Hello World!";
+print OUTPUT localtime;
+close OUTPUT;
+
+
+my $template = $self->load_tmpl($appdir . '/optimizer-results.html');
+$template->param(
+            TITLE  => "Optimizer Status",
+            STILL_RUNNING  => $still_running,
+            RESULT_FILE => "$tmpdir" . 'results.txt'
+        );
+return $template->output;
