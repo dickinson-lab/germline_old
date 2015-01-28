43c5f0441d1aa03bc7bd9ec35afd9bef5d4efe5a
Daniel Dickinson <ddickins@live.unc.edu>
2015-01-28 09:32:45 -0500

    Testing redirect

--- libs/OptimizerMain.pm
+++ libs/OptimizerMain.pm
@@ -59,7 +59,7 @@ sub optimizer_status : Runmode {
         }
     }
 
-    my $template = $self->load_tmpl('/optimizer-results.html');
+    my $template = $self->load_tmpl('http://localhost/optimizer-results.html');
     $template->param(
                 TITLE  => "Optimizer Status",
                 STILL_RUNNING  => $still_running
