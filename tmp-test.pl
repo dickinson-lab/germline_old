b8cd64039a263a8aace080a5d7758f165b68fa61
Daniel Dickinson <ddickins@live.unc.edu>
2015-01-30 16:03:33 -0500

    Test creating and reading a tmp file

--- tmp-test.pl
+++ tmp-test.pl
@@ -8,7 +8,7 @@ use CGI::Carp qw(fatalsToBrowser);
 
 my $datadir = $ENV{OPENSHIFT_DATA_DIR};
 my $tmpdir = $datadir . 'tmp/';
-open OUTPUT, ">", $datadir . 'results.txt' or die "Can't create tmp file";
+open OUTPUT, ">", $tmpdir . 'results.txt' or die "Can't create tmp file";
 print OUTPUT "Hello World!\n";
 print OUTPUT localtime, "\n";
 close OUTPUT;
@@ -17,14 +17,14 @@ my $appdir = $ENV{OPENSHIFT_REPO_DIR};
 my $template = HTML::Template->new( filename => $appdir . '/optimizer-results.html' );
 $template->param(
             TITLE  => "tmp Test",
-            RESULT_FILE => $datadir . 'results.txt'
+            RESULT_FILE => $tmpdir . 'results.txt'
         );
 
 print "Content-Type: text/html\n\n";
 print "<p1>Temp location is $tmpdir</p1>";
 print "<p1>App location is $appdir</p1>";
-print "<p1>Looking for file at " . $datadir . 'results.txt' . "</p1>";
-open TXTFILE, "<", $datadir . 'results.txt' or die "Can't find tmp file";
+print "<p1>Looking for file at " . $tmpdir . 'results.txt' . "</p1>";
+open TXTFILE, "<", $tmpdir . 'results.txt' or die "Can't find tmp file";
 while (my $a = <TEXTFILE>) {
     print "<p1>$a</p1>";
 }
