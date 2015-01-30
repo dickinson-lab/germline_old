6eb721d77fe035ca77b225d5549d6b8a119c9474
Daniel Dickinson <ddickins@live.unc.edu>
2015-01-30 15:39:45 -0500

    Test creating and reading a tmp file

--- tmp-test.pl
+++ tmp-test.pl
@@ -8,7 +8,7 @@ use CGI::Carp qw(fatalsToBrowser);
 
 my $datadir = $ENV{OPENSHIFT_DATA_DIR};
 my $tmpdir = $datadir . 'tmp/';
-open OUTPUT, ">", $tmpdir . 'results.txt';
+open OUTPUT, ">", $tmpdir . 'results.txt' or die "Can't create tmp file";
 print OUTPUT "Hello World!\n";
 print OUTPUT localtime, "\n";
 close OUTPUT;
@@ -24,7 +24,7 @@ print "Content-Type: text/html\n\n";
 print "<p1>Temp location is $tmpdir</p1>";
 print "<p1>App location is $appdir</p1>";
 print "<p1>Looking for file at " . $tmpdir . 'results.txt' . "</p1>";
-open TXTFILE, "<", $tmpdir . 'results.txt' or die "Can't find file";
+open TXTFILE, "<", $tmpdir . 'results.txt' or die "Can't find tmp file";
 while (my $a = <TEXTFILE>) {
     print "<p1>$a</p1>";
 }
