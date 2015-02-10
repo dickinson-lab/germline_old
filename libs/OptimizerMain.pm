07e2ef5cc4efcced98b824019d84e7c8d655d2ce
Daniel Dickinson <ddickins@live.unc.edu>
2015-02-10 08:22:58 -0500

    Debugging

--- libs/OptimizerMain.pm
+++ libs/OptimizerMain.pm
@@ -202,7 +202,7 @@ sub optimizer_results : Runmode {
     #Temporary code
     my $data = "Content-Type: text/html\n\n";
     $data .= $JSONresults . "\n";
-    $data .= "href = $results \n";
+    $data .= "href = $href \n";
     $data .= %results . "\n";
     $data .= $q->param('name') . "\n";
     $data .= $q->param('seq_type') . "\n";
