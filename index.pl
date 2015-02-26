c921e9a75790826a4aea6c8f3281855223c70e8a
Daniel Dickinson <ddickins@live.unc.edu>
2015-02-26 09:05:13 -0500

    A few cosmetic changes

--- index.pl
+++ index.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl
+
 print "Content-type: text/html\r\n\r\n";
 print <<EOF
 <!doctype html>
 <html lang="en">
 <head>
+  <meta http-equiv="refresh" content="0; url=http://germline-ddickins.apps.unc.edu/optimizer.html" >
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title>Welcome to OpenShift</title>
